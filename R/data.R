138391c8c1a817069029cdb2523e315fb1420e65
Arash <45104106+arashHaratian@users.noreply.github.com>
2022-10-04 16:18:12 +0200

    add data and utils.R

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' A random generated data for knapsack problem
+#'
+#' This data has 2 columns, the `w` and `v`. Each row represents the weight and value of an object.
+#'
+#' @format A data frame with 2000 rows and 2 columns:
+#' \describe{
+#'   \item{w}{The weight of the object ranging from 1 to 4000, Integer}
+#'   \item{v}{The value of the object ranging from 0 to 10000, Numeric}
+#' }
+
+"knapsack_objects"
