7867c5d9c27b02269a0e83c17b219c422f08c574
Arash <45104106+arashHaratian@users.noreply.github.com>
2022-10-06 21:35:55 +0200

    add parallel version of brute force

--- R/knapsack_functions.R
+++ R/knapsack_functions.R
@@ -2,6 +2,8 @@
 #'
 #' @param x A `data.frame` containing `w`, `v` representing the weights and values for each item
 #' @param W The weight limit of the knapsack
+#' @param parallel Default to `FALSE`. For running the Parallelize brute force search.
+#'
 #'
 #' @return A list containing the `elements` and the best `value`
 #' @export
@@ -10,7 +12,7 @@
 #' data(knapsack_objects)
 #' brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
 #'
-brute_force_knapsack <- function(x, W) {
+brute_force_knapsack <- function(x, W, parallel = FALSE) {
   stopifnot({
     is.data.frame(x)
     is.numeric(x[[1]])
@@ -20,6 +22,27 @@ brute_force_knapsack <- function(x, W) {
     length(W)==1
     W > 0
   })
+
+  if(parallel){
+    w <- x$w
+    v <- x$v
+    future::plan("multicore", workers = future::availableCores)
+    vals <- furrr::future_map(1:(2^nrow(x)-1), ~{
+      idx <- which(intToBits(.x) == 1)
+      if(sum(w[idx]) <= W){
+        sum(v[idx])
+      }else{
+        0
+      }
+    })
+
+    best <- which.max(unlist(vals))
+    elements <- which(intToBits(best) == 1)
+    return(list(
+      "value" = max(unlist(vals)),
+      "elements" = elements
+    ))
+  }
 
   final <- vector("numeric")
   max <- 0
@@ -65,13 +88,13 @@ knapsack_dynamic <- function(x, W) {
   })
 
   dynamic_knapsack <- function(i, j) {
-    
+
     if (i == 1) {
       return()
     }
     # test <- memoise(max_val)
     # test <- memoise(dynamic_knapsack)
-    
+
     if (m[i, j] > m[i - 1, j]) {
       dynamic_knapsack(i - 1, j - x[i, "w"])
       ln <<- ln + 1
@@ -83,9 +106,9 @@ knapsack_dynamic <- function(x, W) {
     }
   }
   n <- length(rownames(x))
-  
+
   m <- matrix(0, nrow = n, ncol = W)
-  
+
   for(i in 2:n){
     for(j in 1:W){
       if((x[i,"w"] >= j)){
@@ -95,7 +118,7 @@ knapsack_dynamic <- function(x, W) {
       }
     }
   }
-  
+
   elements_knapsack <- rep(0,n)
   ln <- 0
   dynamic_knapsack(n,W)
@@ -132,16 +155,16 @@ greedy_knapsack <- function(x, W) {
     sort(x$percentage,
          index.return = TRUE,
          decreasing = TRUE)$ix
-  
+
   wsum <- 0
   vsum <- 0
   firstnot <- 0
   elements_ix <- c()
-  
+
   wsum2 <- 0
   vsum2 <- 0
   elements_ix2 <- c()
-  
+
   for (i in sorting) {
     if ((firstnot == 0) && (wsum + x[i, "w"]) <= W) {
       wsum <- wsum + x[i, "w"]
@@ -152,7 +175,7 @@ greedy_knapsack <- function(x, W) {
       wsum2 <- wsum2 + x[i, "w"]
       vsum2 <- vsum2 + x[i, "v"]
       elements_ix2 <- append(elements_ix2, i)
-      
+
       ifelse(vsum >= vsum2,
              return(list(
                value = vsum, elements = elements_ix
@@ -185,7 +208,7 @@ greedy_knapsack <- function(x, W) {
 #' @examples
 #' data(knapsack_objects)
 #' greedy_knapsack2(x = knapsack_objects[1:800,], W = 3700)
-#' 
+#'
 greedy_knapsack2 <- function(x, W) {
   stopifnot({
     is.data.frame(x)
@@ -194,9 +217,9 @@ greedy_knapsack2 <- function(x, W) {
     names(x) %in% c("v", "w")
     W > 0
   })
-  
+
   n <- length(rownames(x))
-  
+
   df <- data.frame(x)
   x$percentage <- x[[2]] / x[[1]]
   sorting <-
@@ -223,7 +246,7 @@ greedy_knapsack2 <- function(x, W) {
     for (i in sorting){
       if((firstnot != i) && ((wsum2 + x[i,1])<=W)){
         wsum2 <- wsum2 + x[i,1]
-        vsum2 <- vsum2 + x[i,2] 
+        vsum2 <- vsum2 + x[i,2]
         elements_ix2 <- append(elements_ix2,i)
       }
     }
